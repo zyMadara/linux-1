b8e94f397e2809ba8b7c9994de1e29b8720658a1
Jian Xu <jian.xu@amlogic.com>
2015-12-09 14:28:31 +0800

    PD#115495: audio: add DTS-HD MA format for HDMI raw data pass through
    Change-Id: I9d7886b9f93d65df0885296637df5c0e01fbb46a

--- sound/soc/aml/m8/aml_spdif_dai.c
+++ sound/soc/aml/m8/aml_spdif_dai.c
@@ -73,12 +73,15 @@ struct aml_spdif {
 	int old_samplerate;
 };
 struct aml_spdif *spdif_p;
-/*
+unsigned int clk81 = 0;
+EXPORT_SYMBOL(clk81);
+
 static int iec958buf[32 + 16];
-*/
+static int old_samplerate = -1;
+
 void aml_spdif_play(void)
 {
-#if 0
+#if 1
 	struct _aiu_958_raw_setting_t set;
 	struct _aiu_958_channel_status_t chstat;
 	struct snd_pcm_substream substream;
@@ -96,12 +99,28 @@ void aml_spdif_play(void)
 	set.chan_stat->chstat1_l = 0X200;
 	set.chan_stat->chstat1_r = 0X200;
 	audio_hw_958_enable(0);
-	if (last_iec_clock != AUDIO_CLK_FREQ_48) {
-		ALSA_PRINT("enterd %s,set_clock:%d,sample_rate=%d\n", __func__,
-			   last_iec_clock, AUDIO_CLK_FREQ_48);
-		last_iec_clock = AUDIO_CLK_FREQ_48;
-		audio_set_958_clk(AUDIO_CLK_FREQ_48, AUDIO_CLK_256FS);
+	if (old_samplerate != AUDIO_CLK_FREQ_48) {
+		pr_info("enterd %s,set_clock:%d,sample_rate=%d\n",
+		__func__, old_samplerate, AUDIO_CLK_FREQ_48);
+		old_samplerate = AUDIO_CLK_FREQ_48;
+		aml_set_spdif_clk(48000 * 512, 0);
 	}
+	/* Todo, div can be changed, for most case, div = 2 */
+	/* audio_set_spdif_clk_div(); */
+	/* 958 divisor: 0=no div; 1=div by 2; 2=div by 3; 3=div by 4. */
+	if (IEC958_mode_codec == 4  || IEC958_mode_codec == 5 ||
+	IEC958_mode_codec == 7 || IEC958_mode_codec == 8) {
+		pr_info("set 4x audio clk for 958\n");
+		aml_cbus_update_bits(AIU_CLK_CTRL, 3 << 4, 0 << 4);
+	} else if (0) {
+		pr_info("share the same clock\n");
+		aml_cbus_update_bits(AIU_CLK_CTRL, 3 << 4, 1 << 4);
+	} else {
+		pr_info("set normal 512 fs /4 fs\n");
+		aml_cbus_update_bits(AIU_CLK_CTRL, 3 << 4, 3 << 4);
+	}
+	/* enable 958 divider */
+	aml_cbus_update_bits(AIU_CLK_CTRL, 1 << 1, 1 << 1);
 	audio_util_set_dac_958_format(AUDIO_ALGOUT_DAC_FORMAT_DSP);
 	memset(iec958buf, 0, sizeof(iec958buf));
 	audio_set_958outbuf((virt_to_phys(iec958buf) + 63) & (~63), 128, 0);
@@ -357,7 +376,12 @@ void aml_hw_iec958_init(struct snd_pcm_substream *substream)
 		aml_write_cbus(AIU_958_CHSTAT_L1, 0x900);
 		aml_write_cbus(AIU_958_CHSTAT_R0, 0x1902);
 		aml_write_cbus(AIU_958_CHSTAT_R1, 0x900);
-		aout_notifier_call_chain(AOUT_EVENT_RAWDATA_MAT_MLP, substream);
+		if (IEC958_mode_codec == 8)
+			aout_notifier_call_chain(AOUT_EVENT_RAWDATA_DTS_HD_MA,
+			substream);
+		else
+			aout_notifier_call_chain(AOUT_EVENT_RAWDATA_MAT_MLP,
+			substream);
 	} else {
 		aout_notifier_call_chain(AOUT_EVENT_IEC_60958_PCM, substream);
 	}

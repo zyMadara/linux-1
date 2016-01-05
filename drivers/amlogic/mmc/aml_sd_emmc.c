5063364d1cefacbfb6100bf1badc270566f746fc
Tao Dong <tao.dong@amlogic.com>
2016-01-05 20:13:24 +0800

    PD#117559:EMMC&NAND:merge from trunk by yuyonghui:2016-01-05
    Change-Id: I26ac66ba2943d0620d3473293dbbe3782b652dfa

--- drivers/amlogic/mmc/aml_sd_emmc.c
+++ drivers/amlogic/mmc/aml_sd_emmc.c
@@ -1442,9 +1442,9 @@ void aml_sd_emmc_start_cmd(struct amlsd_platform *pdata,
 
 	/*Add external CMD23 for multi-block operation*/
 #ifdef SD_EMMC_MANUAL_CMD23
-	if (((mrq->cmd->opcode == MMC_WRITE_MULTIPLE_BLOCK)
+	if (((mrq->cmd->opcode == MMC_READ_MULTIPLE_BLOCK)
 		|| (mrq->cmd->opcode == MMC_WRITE_MULTIPLE_BLOCK))
-		&& (mrq->cmd->data)) {
+		&& (mrq->cmd->data) && (mrq->sbc)) {
 		des_cmd_cur = (struct cmd_cfg *)&(desc_cur->cmd_info);
 		/*Command Index*/
 		des_cmd_cur->cmd_index = MMC_SET_BLOCK_COUNT;
@@ -1583,10 +1583,10 @@ void aml_sd_emmc_start_cmd(struct amlsd_platform *pdata,
 
 #endif
 
-#ifndef SD_EMMC_MANUAL_CMD23
+#ifdef SD_EMMC_MANUAL_CMD23
 		if (((mrq->cmd->opcode == MMC_WRITE_MULTIPLE_BLOCK)
 			|| (mrq->cmd->opcode == MMC_READ_MULTIPLE_BLOCK))
-			&& (!host->cmd_is_stop)
+			&& (!host->cmd_is_stop) && (!mrq->sbc)
 			&& !(mrq->cmd->flags & (1 << 30))) {
 
 			/* pr_info("Send stop command here\n"); */
@@ -2813,6 +2813,7 @@ static struct amlsd_host *aml_sd_emmc_init_host(struct amlsd_host *host)
 #endif
 
 	spin_lock_init(&host->mrq_lock);
+	mutex_init(&host->pinmux_lock);
 	host->xfer_step = XFER_INIT;
 
 	INIT_LIST_HEAD(&host->sibling);

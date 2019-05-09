48b38ad34300893ec370f6b6bb57ea4995c36b9b
hmz007 <hmz007@gmail.com>
2019-05-09 16:12:18 +0800

    media: nexell: set `pix.sizeimage' for v4l2 app
    This reverts commit 161657501e16 to fix GStreamer
    ERROR: from element /GstPipeline:pipeline0/GstV4l2Src:v4l2src0:
      Video device did not suggest any buffer size.
    
    Change-Id: I2e1ba362c3c2cecd72aed071c04954c3d74c8346
    Signed-off-by: hmz007 <hmz007@gmail.com>

--- drivers/media/platform/nexell/nx-video.c
+++ drivers/media/platform/nexell/nx-video.c
@@ -845,6 +845,10 @@ static int nx_video_set_format(struct file *file, void *fh,
 					pix->plane_fmt[i+j].sizeimage;
 			}
 		}
+
+	} else if (f->type == V4L2_BUF_TYPE_VIDEO_CAPTURE) {
+		set_plane_size_mmap(frame, &f->fmt.pix.sizeimage);
+		f->fmt.pix.bytesperline = frame->stride[0];
 	}
 
 	return 0;

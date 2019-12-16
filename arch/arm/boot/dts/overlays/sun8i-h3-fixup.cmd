d609d6faf45d0bfbdc3fdd67d71cd0c4fa3f55fb
wuweidong <625769020@qq.com>
2019-12-16 18:16:59 +0800

    ARM: dts: sun8i-h3-nanopi: Add matrix-2.8tft overlay

--- arch/arm/boot/dts/overlays/sun8i-h3-fixup.cmd
+++ arch/arm/boot/dts/overlays/sun8i-h3-fixup.cmd
@@ -21,5 +21,29 @@ for i in ${overlays}; do
         elif test "${uart3/enable}" = "no"; then
             fdt set /soc/serial@01c28c00 status disabled
         fi
+    elif test "${i}" = "tft28"; then
+        if test "${tft28/enable}" = "yes"; then
+            fdt set /soc/spi@01c68000/pitft@0 status okay
+            fdt set /soc/spi@01c68000/pitft-ts@1 status okay
+        elif test "${tft28/enable}" = "no"; then
+            fdt set /soc/spi@01c68000/pitft@0 status disabled
+            fdt set /soc/spi@01c68000/pitft-ts@1 status disabled
+        fi
+
+        if test -n "${tft28/debug}"; then
+            fdt set /soc/spi@01c68000/pitft@0 debug <${tft28/debug}>
+        fi
+
+        if test -n "${tft28/speed}"; then
+            fdt set /soc/spi@01c68000/pitft@0 spi-max-frequency <${tft28/speed}>
+        fi
+
+        if test -n "${tft28/rotate}"; then
+            fdt set /soc/spi@01c68000/pitft@0 rotate <${tft28/rotate}>
+        fi
+
+        if test -n "${tft28/fps}"; then
+            fdt set /soc/spi@01c68000/pitft@0 fps <${tft28/fps}>
+        fi
     fi
 done
